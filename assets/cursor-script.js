7d82ec1fe1882a159b2675136f70cb8a8d17f095
manoj <manojkarthikeya@gmail.com>
2021-11-16 23:08:31 +0530

    integrated features into mainpage

---
+++ assets/cursor-script.js
@@ -0,0 +1,25 @@
+const $Ball = document.querySelector(".invertedcursor");
+const $hoverables = document.querySelectorAll(".hoverable");
+
+document.body.addEventListener("mousemove", onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+	$hoverables[i].addEventListener("mouseenter", onMouseHover);
+	$hoverables[i].addEventListener("mouseleave", onMouseHoverOut);
+}
+function onMouseMove(e) {
+	TweenMax.to($Ball, 0.3, {
+		x: e.pageX - 15,
+		y: e.pageY - 15,
+	});
+}
+function onMouseHover() {
+	TweenMax.to($Ball, 0.3, {
+		scale: 2.4,
+	});
+}
+function onMouseHoverOut() {
+	TweenMax.to($Ball, 0.3, {
+		scale: 1,
+	});
+}
+
