bb8a1981a0a0d3365c04000b2371a2e1df8a237f
manoj <manojkarthikeya@gmail.com>
2021-12-05 22:18:31 +0530

    added store page

--- assets/cursor-script.js
+++ assets/cursor-script.js
@@ -7,18 +7,18 @@ for (let i = 0; i < $hoverables.length; i++) {
 	$hoverables[i].addEventListener("mouseleave", onMouseHoverOut);
 }
 function onMouseMove(e) {
-	TweenMax.to($Ball, 0.3, {
+	TweenMax.to($Ball, 0.4, {
 		x: e.pageX - 15,
 		y: e.pageY - 15,
 	});
 }
 function onMouseHover() {
-	TweenMax.to($Ball, 0.3, {
+	TweenMax.to($Ball, 0.4, {
 		scale: 2.4,
 	});
 }
 function onMouseHoverOut() {
-	TweenMax.to($Ball, 0.3, {
+	TweenMax.to($Ball, 0.4, {
 		scale: 1,
 	});
 }
---
+++ pages/store/app.js
@@ -0,0 +1,57 @@
+const games = [
+    {
+        id: 1 ,
+        name: 'Elden Ring',
+        description: "THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
+        image:'https://image.api.playstation.com/vulcan/ap/rnd/202108/0410/2odx6gpsgR6qQ16YZ7YkEt2B.png',
+        price: 2499.00
+    },
+    {
+        id: 2 ,
+        name: 'Dead Space Remake',
+        description: "You are Isaac Clarke, an engineer on the spacecraft USG Ishimura. You're not a warrior. You're not a soldier. You are, however, the last line of defense for the remaining living crew.",
+        image: 'https://mms.businesswire.com/media/20210722005823/en/893405/5/DS_TEASER_Still_3840x2160_RGB_logo.jpg?download=1',
+        price: 3999.00
+    },
+    {
+        id: 3 ,
+        name: 'Control',
+        description: "Winner of over 80 awards, Control is a visually stunning third-person action-adventure that will keep you on the edge of your seat.",
+        image: 'https://www.escapistmagazine.com/wp-content/uploads/2020/08/Control-702x395.jpg',
+        price: 2999.00
+    },
+    {
+        id: 4,
+        name: 'Dark Souls 3',
+        description: "Dark Souls continues to push the boundaries with the latest, ambitious chapter in the critically-acclaimed and genre-defining series. Prepare yourself and Embrace The Darkness!",
+        image: 'https://cdn.akamai.steamstatic.com/steam/apps/374320/capsule_616x353.jpg?t=1636678210',
+        price: 4299.00
+    },
+    {
+        id: 5 ,
+        name:'Forza Horizon 5',
+        description: "Your Ultimate Horizon Adventure awaits! Explore the vibrant and ever-evolving open world landscapes of Mexico with limitless, fun driving action in hundreds of the world’s greatest cars.",
+        image:'https://cdn.cloudflare.steamstatic.com/steam/apps/1551360/capsule_616x353.jpg?t=1638457209',
+        price: 3499.00
+    },
+    {
+        id: 6 ,
+        name: 'Resident Evil 3',
+        description: "Experience survival horror like never before in the 8th major installment in the Resident Evil franchise - Resident Evil Village. With detailed graphics, intense first-person action and masterful storytelling, the terror has never felt more realistic.",
+        image:"https://wallpapercave.com/wp/wp8737802.png",
+        price: 3499.00
+    }
+]
+
+const desc = document.querySelector(".desc");
+const list = document.querySelector(".right-list");
+const img = document.querySelector(".leftmain");
+
+function listClickHandler(){
+    for(let game of games){
+        console.log(game)
+    }
+}
+
+list.addEventListener("click",listClickHandler)
+
